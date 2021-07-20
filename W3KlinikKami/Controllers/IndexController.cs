36c8df5e04f94ac1cd250355bff152d43e74035b
deden97 <dedenp014@gmail.com>
2021-07-20 17:56:11 +0700

    1.) Menu 'Login' Rev1 perubahan: - pada Controller "Index/Login - post" -     * cek username -> jika terdaftar maka selanjutnya cek password. - pada views tombol "Daftar Akun Baru" mengarah ke "Index/DaftarAkunBaru"
    2.) Menu 'DaftarAkunBaru' Rev1
    perubahan:
    - pada Controller "Index/DaftarAkunBaru - post" -
        * cek username tidak boleh sama.
    - tambah method "Index/DaftarAkunBaru - get" pada controller,
    - Views "DaftarAkunBaru" dipindahkan dari Views "login" dan dibuat "@html.partial("~/Views/Templated/Form/TB_USER.cshtml")".
    - membuat template "Views/Templated/Form/TB_USER.cshtml".

--- W3KlinikKami/Controllers/IndexController.cs
+++ W3KlinikKami/Controllers/IndexController.cs
@@ -21,42 +21,67 @@ namespace W3KlinikKami.Controllers
         {
             if (ModelState.IsValid)
             {
-                // verifikasi 'Username' & 'Password'
-                if (this.db.TB_AKUN.Any(l => l.USERNAME == login.USERNAME && l.PASSWORD_AKUN == login.PASSWORD_AKUN))
+                try
                 {
-                    Session["ID"] = this.db.TB_AKUN.Single(l => l.USERNAME == login.USERNAME && l.PASSWORD_AKUN == login.PASSWORD_AKUN).ID;
-                    Session["JABATAN"] = this.db.TB_USER.Find(Session["ID"]).JABATAN;
-                    return RedirectToAction("Index", "Home");
+                    if (this.db.TB_AKUN.Any(l => l.USERNAME == login.USERNAME)) // jika username terdaftar pada Db
+                    {
+                        if (this.db.TB_AKUN.Any(l => l.USERNAME == login.USERNAME && l.PASSWORD_AKUN == login.PASSWORD_AKUN)) // jika password sesuai dengan username
+                        {
+                            Session["ID"] = this.db.TB_AKUN.Single(l => l.USERNAME == login.USERNAME && l.PASSWORD_AKUN == login.PASSWORD_AKUN).ID;
+                            Session["JABATAN"] = this.db.TB_USER.Find(Session["ID"]).JABATAN;
+                            return RedirectToAction("Index", "Home");
+                        }
+                        else // jika password tidak sesuai dengan username
+                        {
+                            ModelState.AddModelError("PASSWORD_AKUN", "Password Tidak Sesuai");
+                        }
+                    }
+                    else // jika username tidak terdaftar pada Db
+                    {
+                        ModelState.AddModelError("USERNAME", "Username Tidak Terdaftar");
+                    }
                 }
-                else
+                catch (Exception e)
                 {
-                    string msg = "tes";
+                    e.ToString();
                 }
             }
             return View();
         }
 
-        [HttpGet]
-        public JsonResult GetJabatan()
+        public ActionResult DaftarAkunBaru()
         {
-            try
-            {
-                this.db.Configuration.ProxyCreationEnabled = false;
-                return Json(this.db.TB_JABATAN.ToList(), JsonRequestBehavior.AllowGet);
-            }
-            finally
-            {
-                this.db.Configuration.ProxyCreationEnabled = true;
-            }
+            ViewData["JABATAN"] = new SelectList(this.db.TB_JABATAN, "KODE_JABATAN", "JABATAN");
+            return View();
         }
 
         [HttpPost]
         public ActionResult DaftarAkunBaru(TB_USER dt)
         {
-            dt.FOTO = "~/image/profile_user.png";
-            this.db.TB_USER.Add(dt);
-            this.db.SaveChanges();
-            return RedirectToAction("Login");
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if(!this.db.TB_AKUN.Any(d => d.USERNAME == dt.TB_AKUN.USERNAME)) // jika username belum terdaftar
+                    {
+                        dt.FOTO = "~/image/profile_user.png";
+                        this.db.TB_USER.Add(dt);
+                        this.db.SaveChanges();
+                        return RedirectToAction("Login");
+                    }
+                    else // jika username sudah terdaftar
+                    {
+                        ModelState.AddModelError("TB_AKUN.USERNAME", "Username Sudah Digunakan, Gunakan Username Lain.");
+                    }
+                }
+                catch(Exception e)
+                {
+                    e.ToString();
+                }
+            }
+
+            ViewData["JABATAN"] = new SelectList(this.db.TB_JABATAN, "KODE_JABATAN", "JABATAN");
+            return View();
         }
     }
 }
